f978485d226516aa3300d39fdffd9cd9963c1f9d
Matheus Cavalcante Veiga <cavalcante.matheusveiga@gmail.com>
2026-04-05 15:39:46 -0300

    Add DynamoDB table resource for customer registration and update related variables

---
+++ dynamo.tf
@@ -0,0 +1,38 @@
+resource "aws_dynamodb_table" "customer_registration" {
+  name           = "${var.customer_dynamo_table_table}${var.env_name}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = var.read_capacity_units
+  write_capacity = var.write_capacity_units
+  hash_key       = "Id"
+  range_key      = "CreatedAt"
+
+  attribute {
+    name = "Id"
+    type = "S" # STRING - Guid will be stored as string
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "N" # NUMBER - Unix timestamp (epoch seconds)
+  }
+
+  ttl {
+    attribute_name = var.table_ttl_attribute_name
+    enabled        = true
+  }
+
+  tags = {
+    Environment = var.env_name
+    Name        = "${var.customer_dynamo_table_table}${var.env_name}"
+  }
+}
+
+output "customer_table_name" {
+  description = "Customer DynamoDB table name"
+  value       = aws_dynamodb_table.customer.name
+}
+
+output "customer_table_arn" {
+  description = "Customer DynamoDB table ARN"
+  value       = aws_dynamodb_table.customer.arn
+}
--- variables.tf
+++ variables.tf
@@ -22,3 +22,27 @@ variable "data_subnet_cidr" {
   description = "data subnet cidr"
   type        = string
 }
+
+variable "customer_dynamo_table_table" {
+  description = "Customer DynamoDB table name"
+  default     = "customer-registration-"
+  type        = string
+}
+
+variable "read_capacity_units" {
+  description = "DynamoDB read capacity units"
+  type        = number
+  default     = 5
+}
+
+variable "write_capacity_units" {
+  description = "DynamoDB write capacity units"
+  type        = number
+  default     = 5
+}
+
+variable "table_ttl_attribute_name" {
+  description = "DynamoDB TTL attribute name"
+  type        = string
+  default     = "ExpiresAt"
+}
