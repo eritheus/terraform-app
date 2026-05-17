d7f80728ec7fee0232ee00179ae8e612cb91a05b
Matheus Cavalcante Veiga <cavalcante.matheusveiga@gmail.com>
2026-05-17 14:42:01 -0300

    Fix source URL formatting in ecs_cluster and ecs_app modules

--- ecs.tf
+++ ecs.tf
@@ -1,22 +1,22 @@
 module "ecs_cluster" {
-  source = "git::https://github.com/eritheus/terraform-modules.git/ecs-cluster?ref=main"
+  source = "git::https://github.com/eritheus/terraform-modules.git//ecs-cluster?ref=main"
   name   = "registration"
 }
 
 module "ecs_app_customer_registration" {
-  source       = "git::https://github.com/eritheus/terraform-modules.git/ecs-app?ref=main"
+  source       = "git::https://github.com/eritheus/terraform-modules.git//ecs-app?ref=main"
   cluster_name = module.ecs_cluster.name
   app_name     = "customer-registration"
   image_name   = "nginx:latest"
 
-  depends_on = [ module.ecs_cluster ]
+  depends_on = [module.ecs_cluster]
 }
 
 module "ecs_app_product_registration" {
-  source       = "git::https://github.com/eritheus/terraform-modules.git/ecs-app?ref=main"
+  source       = "git::https://github.com/eritheus/terraform-modules.git//ecs-app?ref=main"
   cluster_name = module.ecs_cluster.name
   app_name     = "product-registration"
   image_name   = "nginx:latest"
-  
-  depends_on = [ module.ecs_cluster ]
+
+  depends_on = [module.ecs_cluster]
 }
