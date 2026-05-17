a0cce13e08d49cc4d8522ecd85c23fa33cc335a7
Matheus Cavalcante Veiga <cavalcante.matheusveiga@gmail.com>
2026-05-17 14:38:32 -0300

    Refactor ecs_app_cluster module to ecs_cluster and update app configurations

--- ecs.tf
+++ ecs.tf
@@ -1,4 +1,22 @@
-module "ecs_app_cluster" {
-  source = "git::https://github.com/eritheus/terraform-modules.git//ecs?ref=main"
-  name   = "customer-registration"
+module "ecs_cluster" {
+  source = "git::https://github.com/eritheus/terraform-modules.git/ecs-cluster?ref=main"
+  name   = "registration"
+}
+
+module "ecs_app_customer_registration" {
+  source       = "git::https://github.com/eritheus/terraform-modules.git/ecs-app?ref=main"
+  cluster_name = module.ecs_cluster.name
+  app_name     = "customer-registration"
+  image_name   = "nginx:latest"
+
+  depends_on = [ module.ecs_cluster ]
+}
+
+module "ecs_app_product_registration" {
+  source       = "git::https://github.com/eritheus/terraform-modules.git/ecs-app?ref=main"
+  cluster_name = module.ecs_cluster.name
+  app_name     = "product-registration"
+  image_name   = "nginx:latest"
+  
+  depends_on = [ module.ecs_cluster ]
 }
